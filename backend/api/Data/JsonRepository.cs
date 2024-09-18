9d18310fd13a2417cc03318c2e58e2310c32208a
kinloveko <delacruzk4817@gmail.com>
2024-09-18 21:13:19 +0800

    added Data and json file

---
+++ backend/api/Data/JsonRepository.cs
@@ -0,0 +1,67 @@
+﻿using backend.Models;
+using System.Text.Json;
+
+namespace backend.Data
+{
+    public class JsonRepository
+    {
+        public List<Contact> Contacts { get; set; }
+
+        //create a constructor that reads the contact.json file content and stores it to Contacts property
+        public JsonRepository()
+        {
+            string json = File.ReadAllText("contacts.json");
+            Contacts = JsonSerializer.Deserialize<List<Contact>>(json);
+        }
+
+        //CRUD
+
+        //create a method that returns all contacts
+        public List<Contact> GetContacts()
+        {
+            return Contacts;
+        }
+
+        //create a method that returns a contact by id
+        public Contact GetContactById(int id)
+        {
+            return Contacts.FirstOrDefault(c => c.Id == id);
+        }
+
+        //create a method that adds a contact
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        //Create a method to update contact 
+        public void UpdateContact(Contact contact) {
+            var existingContact = Contacts.FirstOrDefault(c => c.Id == contact.Id);
+            if (existingContact != null)
+            {
+                existingContact.FirstName = contact.FirstName;
+                existingContact.LastName = contact.LastName;
+                existingContact.PhoneNumber = contact.PhoneNumber;
+                existingContact.Address = contact.Address;
+            }
+        }
+
+        //create a method that deletes a contact
+        public void DeleteContact(int id)
+        {
+            var contact = Contacts.FirstOrDefault(c => c.Id == id);
+            if (contact != null)
+            {
+                Contacts.Remove(contact);
+            }
+        }
+
+        //create a method that saves the contacts to the json file
+        public void SaveContacts()
+        {
+            string json = JsonSerializer.Serialize(Contacts);
+            File.WriteAllText("contacts.json", json);
+        }
+
+    }
+}
